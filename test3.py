5de59607c2219e0ca97c6cc6ecf34b0e156de257
marhoy <martin.hoy@pvv.ntnu.no>
2017-01-19 21:45:30 +0100

    Inital commits

--- test3.py
+++ test3.py
@@ -3,30 +3,45 @@ import time
 import random
 
 
-def worker(item):
-    print('Starting working on item', item.value)
-    number = random.randint(2, 8)
-    time.sleep(number)
-    item.result = number
-    print('Finished working on item', item.value)
+def worker(item, queue):
+    print('Starting working on item', item.number, queue)
+    for i in range(item.work):
+#        queue.put(item.number, i)
+        time.sleep(1)
+    print('Finished working on item {} after {} seconds'.format(item.number, item.work))
 
 
 class Item:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, number, work):
+        self.number = number
+        self.work = work
+        self.progess = 0
+
 
 if __name__ == '__main__':
 
-    items = []
-    for i in range(10):
-        items.append(Item(i))
+    work_items = []
+    total_workload = 0
+    for i in range(6):
+        work = random.randint(5, 10)
+        work_items.append(Item(i, work))
+        total_workload += work
+
+    print('Total workload: ', total_workload)
 
     print('Starting workers')
+    queue = multiprocessing.Queue()
     with multiprocessing.Pool(processes=4) as pool:
+        result = pool.map_async(worker, (work_items, 2))
 
-        # pool.map(worker, items)
-        for item in items:
-            result = pool.apply_async(worker, item)
-        print('After apply-loop', result)
+        while not result.ready():
+#            item, progress = queue.get()
+#            work_items[item].progress = progress
 
+            total_progress = 0
+            for item in work_items:
+                total_progress += item.progess
+            time.sleep(1)
+            print('Total progress: ', total_progress)
+        print(result.get())
     print('All workers finished')
