d409806c1f25fa168927ffdc7656a436a946abef
marhoy <martin.hoy@pvv.ntnu.no>
2017-01-20 13:06:19 +0100

    Initial commits

--- parallel-testing.py
+++ parallel-testing.py
@@ -3,19 +3,23 @@ import time
 import random
 
 
-def worker(item, queue):
-    print('Starting working on item', item.number, queue)
+def worker(item):
+    print('Starting working on item', item.number)
     for i in range(item.work):
-#        queue.put(item.number, i)
         time.sleep(1)
+        worker.queue.put([item.number, i])
     print('Finished working on item {} after {} seconds'.format(item.number, item.work))
+
+
+def worker_init(queue):
+    worker.queue = queue
 
 
 class Item:
     def __init__(self, number, work):
         self.number = number
         self.work = work
-        self.progess = 0
+        self.progress = 0
 
 
 if __name__ == '__main__':
@@ -23,7 +27,7 @@ if __name__ == '__main__':
     work_items = []
     total_workload = 0
     for i in range(6):
-        work = random.randint(5, 10)
+        work = random.randint(1, 3)
         work_items.append(Item(i, work))
         total_workload += work
 
@@ -31,17 +35,17 @@ if __name__ == '__main__':
 
     print('Starting workers')
     queue = multiprocessing.Queue()
-    with multiprocessing.Pool(processes=4) as pool:
-        result = pool.map_async(worker, (work_items, 2))
+    with multiprocessing.Pool(processes=4, initializer=worker_init, initargs=[queue]) as pool:
+        result = pool.map_async(worker, work_items)
 
         while not result.ready():
-#            item, progress = queue.get()
-#            work_items[item].progress = progress
+            i, progress = queue.get(block=True, timeout=2)
+            work_items[i].progress = progress
 
             total_progress = 0
             for item in work_items:
-                total_progress += item.progess
-            time.sleep(1)
+                total_progress += item.progress
+#            time.sleep(1)
             print('Total progress: ', total_progress)
         print(result.get())
     print('All workers finished')
