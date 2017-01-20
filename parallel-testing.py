a2cdeebd5937bab6167281cf9080f4cce3e10ab7
marhoy <martin.hoy@pvv.ntnu.no>
2017-01-20 15:34:32 +0100

    Initial commits

--- parallel-testing.py
+++ parallel-testing.py
@@ -1,18 +1,20 @@
 import multiprocessing
 import time
 import random
+import tqdm
+
+import logging
+
+logging.basicConfig(format='{levelname}: {message}', level=logging.INFO, style='{')
+LOG = logging.getLogger()
 
 
 def worker(item):
-    print('Starting working on item', item.number)
+    LOG.debug('Starting working on item {}'.format(item.number))
     for i in range(item.work):
         time.sleep(1)
-        worker.queue.put([item.number, i])
-    print('Finished working on item {} after {} seconds'.format(item.number, item.work))
-
-
-def worker_init(queue):
-    worker.queue = queue
+        queue.put([item.number, i+1])
+    LOG.debug('Finished working on item {} after {} seconds'.format(item.number, item.work))
 
 
 class Item:
@@ -27,25 +29,31 @@ if __name__ == '__main__':
     work_items = []
     total_workload = 0
     for i in range(6):
-        work = random.randint(1, 3)
+        work = random.randint(2, 6)
         work_items.append(Item(i, work))
         total_workload += work
+    LOG.debug('Total workload: {}'.format(total_workload))
 
-    print('Total workload: ', total_workload)
-
-    print('Starting workers')
+    progress_bar = tqdm.tqdm(total=total_workload, unit='s')
+    total_progress = 0
     queue = multiprocessing.Queue()
-    with multiprocessing.Pool(processes=4, initializer=worker_init, initargs=[queue]) as pool:
+    with multiprocessing.Pool(processes=4) as pool:
+        LOG.debug('Starting workers')
         result = pool.map_async(worker, work_items)
 
-        while not result.ready():
-            i, progress = queue.get(block=True, timeout=2)
-            work_items[i].progress = progress
+        while True:
+            try:
+                i, progress = queue.get(block=True, timeout=2)
+                work_items[i].progress = progress
+            except Exception as e:
+                LOG.debug('Queue is empty, got {}'.format(e))
+                LOG.debug('Result is ready: {}'.format(result.get()))
+                break
 
+            previous_progress = total_progress
             total_progress = 0
             for item in work_items:
                 total_progress += item.progress
-#            time.sleep(1)
-            print('Total progress: ', total_progress)
-        print(result.get())
+            progress_bar.update(total_progress - previous_progress)
+        progress_bar.close()
     print('All workers finished')
