dd369d243ada22637a5664d5583d393e190f69fa
marhoy <martin.hoy@pvv.ntnu.no>
2017-01-31 21:52:29 +0100

    Initial commits

--- nrkdownload/__init__.py
+++ nrkdownload/__init__.py
@@ -1,3 +1,4 @@
 from .constants import *
 from .nrktv import search, ask_for_program_download
 from .utils import search_from_cmdline, get_integer_input
+
---
+++ setup.py
@@ -0,0 +1,92 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+from setuptools import setup
+
+setup(
+    name='nrkdownload',
+
+    # Version number is automatically extracted from Git
+    # https://pypi.python.org/pypi/setuptools_scm
+    # https://packaging.python.org/en/latest/single_source_version.html
+    use_scm_version=True,
+
+    description='Download series or programs from NRK, complete with images and subtitles',
+    long_description=open('README.rst', encoding='UTF-8').read(),
+
+    # The project's main homepage.
+    url='https://github.com/marhoy/nrk-download',
+
+    # Author details
+    author='Martin Høy',
+    author_email='martin.hoy@pvv.ntnu.no',
+
+    # Choose your license
+    license='GPL3',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='sample setuptools development',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=['nrkdownload'],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    setup_requires=['setuptools_scm'],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['requests', 'tqdm'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'nrkdownload=nrkdownload.nrkdownload:main',
+        ]
+    },
+)
