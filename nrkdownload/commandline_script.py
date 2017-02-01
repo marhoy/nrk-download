e7b861261067259036c862ab73f41b973c4fda6d
marhoy <martin.hoy@pvv.ntnu.no>
2017-02-01 19:47:36 +0100

    Initial commits

--- nrkdownload/commandline_script.py
+++ nrkdownload/commandline_script.py
@@ -3,12 +3,10 @@
 import nrkdownload
 import argparse
 import os
+from setuptools_scm import get_version
 
 
 def main():
-    from setuptools_scm import get_version
-    version = get_version(root='.', relative_to=__file__)
-
     parser = argparse.ArgumentParser(description='Download series or programs from NRK, complete with images and subtitles.')
     group1 = parser.add_mutually_exclusive_group(required=True)
     group1.add_argument('-s', '--series', action='store_true', help='Search for series')
@@ -16,7 +14,7 @@ def main():
     parser.add_argument('search_string')
     parser.add_argument('-d', '--dir', metavar='DIRECTORY', action='store',
                         help='The directory where the downloaded files will be placed')
-    group1.add_argument('--version', action='version', version='%(prog)s version ' + version)
+    group1.add_argument('--version', action='version', version='%(prog)s version ' + get_version())
     arguments = parser.parse_args()
 
     if arguments.dir:
