5707eb5d6a6c9ba242ba3ed508ff7adc36e670d0
marhoy <martin.hoy@pvv.ntnu.no>
2017-05-02 20:39:25 +0200

    Adding tests

--- nrkdownload/commandline_script.py
+++ nrkdownload/commandline_script.py
@@ -14,21 +14,20 @@ def main():
         epilog='The files are by default downloaded to ~/Downloads/nrkdownload.'
                ' This can be changed by using the option -d as described above,'
                ' or you can define the environment variable NRKDOWNLOAD_DIR')
+
     parser.add_argument('--version', action='version', version='%(prog)s version ' + version)
     parser.add_argument('-d', metavar='DIRECTORY', action='store',
                         help='The directory where the downloaded files will be placed')
-    group1 = parser.add_mutually_exclusive_group(required=True)
-    group1.add_argument('-s', '--series', action='store_true', help='Search for series')
-    group1.add_argument('-p', '--program', action='store_true', help='Search for programs')
-    group1.add_argument('-u', '--url', action='store_true', help='The search_string is an URL')
-    parser.add_argument_group('search_string', help='String')
-    parser.add_argument('input', type=argparse.FileType('r'), default='-')
-    #parser.add_argument('search_string',
-    #                    help='Whatever you want to search for. Surround the string with single or'
-    #                         ' double quotes if the string contains several words.')
+
+    mutex = parser.add_mutually_exclusive_group(required=True)
+    mutex.add_argument('-p', '--program',
+                       help="Search for a program that matches the string PROGRAM. Interactive download.")
+    mutex.add_argument('-s', '--series',
+                       help="Search for a series that matches the string SERIES. Interactive download")
+    mutex.add_argument('-u', '--url',
+                       help="Download whatever is specified by URLs. No questions asked.")
 
     arguments = parser.parse_args()
-    print(arguments)
 
     if arguments.d:
         nrkdownload.DOWNLOAD_DIR = os.path.expanduser(arguments.d)
@@ -37,7 +36,8 @@ def main():
         nrkdownload.search_from_cmdline(arguments)
 
     if arguments.url:
-        nrkdownload.parse_url(arguments)
+        nrkdownload.parse_urls(arguments)
+
 
 if __name__ == '__main__':
     main()
