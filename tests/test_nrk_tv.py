413a8c0dd5e66e065e9400195b8f55e59d684449
Martin Høy <marhoy@gmail.com>
2022-04-24 22:44:50 +0200

    Replace tests

---
+++ tests/test_nrk_tv.py
@@ -0,0 +1,18 @@
+from nrkdownload.nrk_tv import TVProgram, TVSeries
+
+
+def test_tv_series():
+    series = TVSeries.from_series_id("kongen-av-gulset")
+    assert series.title == "Kongen av Gulset"
+    assert len(series.season_info) == 2
+
+
+def test_tv_season():
+    series = TVSeries.from_series_id("kongen-av-gulset")
+    season = series.get_season(series.season_info[0].season_id)
+    assert len(season.episodes) == 7
+
+
+def test_tv_program():
+    program = TVProgram.from_program_id("MSUI14009516")
+    assert program.title == "Røverrotta"
