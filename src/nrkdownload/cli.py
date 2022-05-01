4031fafe140a6c5786c3c2f42cc855a80c31cdf9
Martin Høy <marhoy@gmail.com>
2022-05-01 15:46:52 +0200

    Enable episode selection

--- src/nrkdownload/cli.py
+++ src/nrkdownload/cli.py
@@ -1,15 +1,17 @@
-import os
 import re
 import sys
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import pkg_resources
 import typer
-from halo import Halo
 from loguru import logger
 
-from nrkdownload.nrk_tv import NotPlayableError, TVProgram, TVSeries, TVSeriesType
+from nrkdownload.download import (
+    download_program,
+    download_series,
+    get_default_dowload_dir,
+)
 
 app = typer.Typer(add_completion=False)
 
@@ -26,64 +28,25 @@ def version_callback(value: bool) -> None:
     if value:
         version = pkg_resources.require("nrkdownload")[0].version
         typer.echo(f"nrkdownload version: {version}")
-        raise typer.Exit()
+        raise typer.Exit(code=0)
 
 
-def download_series(
-    series_id: str, download_dir: Path, only_season_id: Optional[str] = None
-) -> None:
-    """_summary_
-
-    Args:
-        series_id (_type_): _description_
-        download_dir (_type_): _description_
-        only_seasons (_type_, optional): _description_. Defaults to None.
-    """
-    series = TVSeries.from_series_id(series_id)
-    typer.echo(f"Downloading {series.title}")
-    series.download_images(download_dir)
-    for info in series.season_info:
-        if (only_season_id is not None) and (info.season_id != only_season_id):
-            continue
-        season = series.get_season(info.season_id)
-        typer.echo(f"Downloading {season.title}")
-        season.download_images(download_dir / series.dirname)
-        directory = download_dir / series.dirname / season.dirname
-        with typer.progressbar(season.episodes) as episodes:
-            for episode_number, program_id in enumerate(episodes, start=1):
-                try:
-                    program = TVProgram.from_program_id(program_id)
-                except NotPlayableError as e:
-                    typer.echo(f"Skipping: {e}")
-                    continue
-                if series.type == TVSeriesType.sequential:
-                    sequence_string = f"s{season.season_id:>02s}e{episode_number:>02d}"
-                else:
-                    sequence_string = season.season_id
-                program.download_as_episode(series.title, sequence_string, directory)
-                # typer.echo(f"Downloading episode {program.title}")
+def match_program_url(url: str) -> Optional[str]:
+    if match := re.match(r"https://tv.nrk.no/program/(\w+)", url):
+        return match.group(1)
+    return None
 
 
-def download_program(program_id: str, download_dir: Path) -> None:
-    """_summary_
-
-    Args:
-        program_id (_type_): _description_
-        download_dir (_type_): _description_
-    """
-    try:
-        program = TVProgram.from_program_id(program_id)
-    except NotPlayableError as e:
-        typer.echo(f"Skipping: {e}")
-        return
-    with Halo(text=f"{program.title}", spinner="dots") as spinner:
-        program.download_as_program(download_dir / program.title)
-        spinner.succeed()
-
-
-def get_default_dowload_dir() -> Path:
-    dir = os.environ.get("NRKDOWNLOAD_DIR") or Path.home() / "Downloads" / "nrkdownload"
-    return Path(dir)
+def match_series_url(url: str) -> Optional[Tuple[str, Optional[str], Optional[str]]]:
+    if match := re.match(
+        r"https://tv.nrk.no/serie/([\w-]+)(?:/sesong/(\w+)|/(\w+)|)(?:/episode/(\w+)|/(\w+)|)",  # noqa: E501
+        url,
+    ):
+        series_id = match.group(1)
+        season_id = match.group(2) or match.group(3)
+        episode_id = match.group(4) or match.group(5)
+        return series_id, season_id, episode_id
+    return None
 
 
 @app.command()
@@ -115,37 +78,15 @@ def main(
         help="Increase logger verbosity. Can be repeated up to four times.",
         max=4,
         clamp=True,
+        is_eager=True,
     ),
 ) -> None:
     for url in urls:
-        # Example program URL:
-        # https://tv.nrk.no/program/KOID75000320
-        #
-        # Example "sequential" series URL:
-        # https://tv.nrk.no/serie/kongen-av-gulset
-        # https://tv.nrk.no/serie/kongen-av-gulset/sesong/1
-        # https://tv.nrk.no/serie/kongen-av-gulset/sesong/1/episode/3/avspiller
-        #
-        # Example "standard" series URL:
-        # https://tv.nrk.no/serie/klassequizen
-        # https://tv.nrk.no/serie/klassequizen/2021
-        # https://tv.nrk.no/serie/klassequizen/2021/DSRR21000521/avspiller
-        #
-        # Example "news" series URL:
-        # https://tv.nrk.no/serie/dagsrevyen-21
-        # https://tv.nrk.no/serie/dagsrevyen-21/202203
-        # https://tv.nrk.no/serie/dagsrevyen-21/202203/NNFA21030122/avspiller
-
-        if match := re.match(
-            r"https://tv.nrk.no/serie/([\w-]+)(?:/sesong/(\w+)|/(\w+)|)", url
-        ):
-            series_id = match.group(1)
-            only_season_id = match.group(2) or match.group(3)
-            download_series(series_id, download_dir, only_season_id)
-
-        elif match := re.match(r"https://tv.nrk.no/program/(\w+)", url):
-            program_id = match.group(1)
-            download_program(program_id, download_dir)
+        if program_id := match_program_url(url):
+            download_program(download_dir, program_id)
+        elif match := match_series_url(url):
+            series_id, season_id, episode_id = match
+            download_series(download_dir, series_id, season_id, episode_id)
         else:
             typer.echo("Not able to parse URL")
-            typer.Exit(1)
+            raise typer.Exit(code=1)
---
+++ src/nrkdownload/download.py
@@ -0,0 +1,91 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+import typer
+from halo import Halo
+from loguru import logger
+
+from nrkdownload.nrk_tv import NotPlayableError, TVProgram, TVSeries, TVSeriesType
+
+
+def get_default_dowload_dir() -> Path:
+    dir = os.environ.get("NRKDOWNLOAD_DIR") or Path.home() / "Downloads" / "nrkdownload"
+    return Path(dir)
+
+
+def download_series(
+    download_dir: Path,
+    series_id: str,
+    only_season_id: Optional[str] = None,
+    only_episode_id: Optional[str] = None,
+) -> None:
+    """_summary_
+
+    Args:
+        series_id (_type_): _description_
+        download_dir (_type_): _description_
+        only_seasons (_type_, optional): _description_. Defaults to None.
+    """
+    series = TVSeries.from_series_id(series_id)
+    typer.echo(f"Downloading {series.title}")
+    series.download_images(download_dir)
+    for info in series.season_info:
+        if (only_season_id is not None) and (info.season_id != only_season_id):
+            logger.debug(f"Skipping season number {info.season_id}...")
+            continue
+
+        season = series.get_season(info.season_id)
+        typer.echo(f"Downloading {season.title}")
+        season.download_images(download_dir / series.dirname)
+
+        directory = download_dir / series.dirname / season.dirname
+        with typer.progressbar(season.episodes) as episodes:
+            for episode_number, program_id in enumerate(episodes, start=1):
+
+                # If we're asked to only download one episode, possibly skip this one.
+                # If it's a sequential series, only_episode_id is an episode number.
+                # Otherwise, only_episode_id is a program_id.
+                if only_episode_id is not None:
+                    if (series.type == TVSeriesType.sequential) and (
+                        only_episode_id != str(episode_number)
+                    ):
+                        logger.debug(f"Skipping episode number {episode_number}...")
+                        continue
+                    elif (
+                        (series.type == TVSeriesType.news)
+                        or (series.type == TVSeriesType.standard)
+                    ) and (only_episode_id != program_id):
+                        logger.debug(f"Skipping episode ID {program_id}...")
+                        continue
+
+                try:
+                    program = TVProgram.from_program_id(program_id)
+                except NotPlayableError as e:
+                    typer.echo(f"Skipping: {e}")
+                    continue
+
+                if series.type == TVSeriesType.sequential:
+                    sequence_string = f"s{season.season_id:>02s}e{episode_number:>02d}"
+                else:
+                    sequence_string = season.season_id
+
+                program.download_as_episode(series.title, sequence_string, directory)
+                # typer.echo(f"Downloading episode {program.title}")
+
+
+def download_program(download_dir: Path, program_id: str) -> None:
+    """_summary_
+
+    Args:
+        program_id (_type_): _description_
+        download_dir (_type_): _description_
+    """
+    try:
+        program = TVProgram.from_program_id(program_id)
+    except NotPlayableError as e:
+        typer.echo(f"Skipping: {e}")
+        return
+    with Halo(text=f"{program.title}", spinner="dots") as spinner:
+        program.download_as_program(download_dir / program.title)
+        spinner.succeed()
